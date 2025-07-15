8b4c63a8566d05e59ba9e661169e749a3b2a6464
Melvin Vergel <melvin.vergel@gmail.com>
2025-07-15 15:21:38 -0400

    add agent

--- ws-api-deployment-v2.sh
+++ ws-api-deployment-v2.sh
@@ -179,6 +179,18 @@ docker run -d \
   --network bridge \
   ws-site
 
+
+echo "Adding Portainer Agent"
+remove_container_if_exists portainer_agent
+docker run -d \
+  -p 9001:9001 \
+  --name portainer_agent \
+  --restart=always \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /var/lib/docker/volumes:/var/lib/docker/volumes \
+  -v /:/host \
+  portainer/agent:2.27.3
+
 # -----------------------------------------------------------------------------
 # FINAL NOTIFICATION
 # -----------------------------------------------------------------------------
